e01b3fcc1fcf0222dd454afccce5bf7f7829d4ae
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2020-08-04 03:30:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Count characters in your string and return a string object
+function count(string){
+let result=new Object();
+for(let i=0;i<string.length;i++){
+  if(result[string[i]]===undefined){
+    result[string[i]]=1;
+  }
+  else{
+    let count=result[string[i]];
+    count++;
+    result[string[i]]=count;
+  }
+}
+return result;
+}
+console.log(count("abcdefghijklmnopqrstuvwxyz"));
+console.log(count("Hellohowareyou"));
+console.log(count(""));
